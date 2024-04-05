37ce6cbfaa2d1f4541f49fde172e0dcb7e364ef5
Daorse <mmorpgplays5@gmail.com>
2024-04-05 12:20:10 +0200

    AfterCreate page fait

---
+++ AfterCreate/AfterCreate.js
@@ -0,0 +1,11 @@
+let modif = document.querySelector('.modifcation')
+let Valid = document.querySelector('.valider')
+
+
+modif.addEventListener('click', () => {
+    window.location.href = "../CreatePoste/Create.html"
+})
+
+Valid.addEventListener('click', () => {
+    window.location.href = "../Scroll-Page/Scroll.html"
+})
--- CreatePoste/Create.js
+++ CreatePoste/Create.js
@@ -1,5 +1,5 @@
 let Create = document.querySelector('.Create');
 
 Create.addEventListener('click', () => {
-    window.location.href = "./AfterCreate/AfterCreate.html"
+    window.location.href = "../AfterCreate/AfterCreate.html"
 })

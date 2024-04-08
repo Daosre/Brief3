48327d55856847a75fd6fbfc9e1d9f8a44377e91
Daorse <mmorpgplays5@gmail.com>
2024-04-08 10:59:41 +0200

    .

--- CreatePoste/Create.js
+++ CreatePoste/Create.js
@@ -1,5 +1,44 @@
-let Create = document.querySelector('.Create');
+async function CreatePost() {
+    let title = document.querySelector('.title').value
+    let image = document.querySelector('.Img').value
+    let location = document.querySelector('.location').value
+    let price = document.querySelector('.price').value
+    let description = document.querySelector('.message').value
+    let error = document.querySelector('.error')
 
-Create.addEventListener('click', () => {
-    window.location.href = "../AfterCreate/AfterCreate.html"
-})
+    let addArticles = {
+        title : title,
+        image : image,
+        description : description,
+        location : location,
+        price : price
+    } 
+    let request = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json; charset=utf-8',
+        },
+        body: JSON.stringify(addArticles)
+    }
+
+    if(title === '') {
+        error.innerHTML = 'Missing Field'
+    }
+    if(image === '') {
+        error.innerHTML = 'Missing Field'
+    }
+    if(description === '') {
+        error.innerHTML = 'Missing Field'
+    }
+    if(location === '') {
+        error.innerHTML = 'Missing Field'
+    } 
+    if(price === '') {
+        error.innerHTML = 'Missing Field'
+    } else {
+        let apiRequest = await fetch('http://127.0.0.1:4529/addArticles', request)
+        let reponse = await apiRequest
+        console.log(reponse)
+        window.location.href = "../AfterCreate/AfterCreate.html"
+    }
+    }
